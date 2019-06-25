67eeca96749a1e760948e7373c00ba4c09cb0007
jiangge <jiangge@in66.com>
2019-06-25 17:03:06 +0800

    00

---
+++ Thrift.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jangozw
+ * Date: 2019-06-25
+ * Time: 16:58
+ */
+
+class Thrift
+{
+    public static function test(){
+        return 'aaaa';
+    }
+}
